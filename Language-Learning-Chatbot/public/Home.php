f70a2535aa2b393f5ca00df67ed6cac23b2fac1d
LauraLucasZ <lauralucas060@gmail.com>
2024-10-18 21:51:40 +0300

    path change

--- Language-Learning-Chatbot/public/Home.php
+++ Language-Learning-Chatbot/public/Home.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chatrock</title>
-    <link rel="stylesheet" href="Stylehome.css">
+    <link rel="stylesheet" href="css/Stylehome.css">
 </head>
 <body>
     <div class="container">
