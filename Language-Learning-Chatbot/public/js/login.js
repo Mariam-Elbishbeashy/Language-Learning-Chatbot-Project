594241e050f96b6965abef96bd4be03e82fac203
SondosGafar <sondos2202126@miuegypt.edu.eg>
2024-10-23 18:57:15 +0300

    Updated the login page

--- Language-Learning-Chatbot/public/js/login.js
+++ Language-Learning-Chatbot/public/js/login.js
@@ -4,14 +4,12 @@ window.onload = function() {
     const flipElement = document.getElementById('flip');
     const container = document.querySelector('.container');
   
-    // Disable animation initially
     container.classList.add('no-animation');
   
     if (isSignup === 'true') {
-      flipElement.checked = true; // Flip to sign-up form
+      flipElement.checked = true; 
     }
   
-    // Remove the no-animation class once a user clicks to toggle forms
     flipElement.addEventListener('change', function() {
       container.classList.remove('no-animation');
     });
