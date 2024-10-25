78699308f305156690cfea8708cea4cb6bce0831
LauraLucasZ <lauralucas060@gmail.com>
2024-10-25 18:33:12 +0300

    path edit

--- Language-Learning-Chatbot/public/Home.php
+++ Language-Learning-Chatbot/public/Home.php
@@ -26,7 +26,7 @@
                     </a>
                 </li>
                 <li>
-                    <a href="gamification.php">
+                    <a href="game.php">
                         <span class="icon">
                             <i class="bi bi-controller"></i>
                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-controller" viewBox="0 0 16 16">
