915d823a52c32b73841a2884d2bfa0af231a30d4
MohamedIhabIbrahim22 <168291140+MohamedIhabIbrahim22@users.noreply.github.com>
2024-10-30 22:05:31 +0300

    Changes done

--- Language-Learning-Chatbot/modules/llchatbot_db.sql
+++ Language-Learning-Chatbot/modules/llchatbot_db.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `llchatbot_db`
+-- Database: `llchatbot_db.sql`
 --
 
 -- --------------------------------------------------------
