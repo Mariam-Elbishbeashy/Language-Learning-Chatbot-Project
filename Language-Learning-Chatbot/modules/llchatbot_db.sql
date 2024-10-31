33d58c68d6ecdf5cfae80ba751b4527cfa0cccab
Mariam Elbishbeashy <mariam.elbishbeashy@gmail.com>
2024-10-31 03:11:13 +0300

    updated database

--- Language-Learning-Chatbot/modules/llchatbot_db.sql
+++ Language-Learning-Chatbot/modules/llchatbot_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 29, 2024 at 09:43 PM
+-- Generation Time: Oct 31, 2024 at 01:10 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -11,18 +11,21 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `llchatbot_db.sql`
+-- Database: `llchatbot_db`
 --
 
 -- --------------------------------------------------------
 
+--
 -- Table structure for table `activities`
+--
 
 CREATE TABLE `activities` (
   `activity_id` int(11) NOT NULL,
@@ -31,7 +34,9 @@ CREATE TABLE `activities` (
 
 -- --------------------------------------------------------
 
+--
 -- Table structure for table `challenge`
+--
 
 CREATE TABLE `challenge` (
   `challenge_id` int(11) NOT NULL,
@@ -43,7 +48,36 @@ CREATE TABLE `challenge` (
 
 -- --------------------------------------------------------
 
+--
+-- Table structure for table `chathistory`
+--
+
+CREATE TABLE `chathistory` (
+  `chathistory_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `language_analysis`
+--
+
+CREATE TABLE `language_analysis` (
+  `languageAnalysis_id` int(11) NOT NULL,
+  `language_name` varchar(50) NOT NULL,
+  `usage_percentage` decimal(5,2) NOT NULL,
+  `common_topics` varchar(255) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `quiz_questions`
+--
 
 CREATE TABLE `quiz_questions` (
   `question_id` int(11) NOT NULL,
@@ -60,13 +94,16 @@ CREATE TABLE `quiz_questions` (
 
 -- --------------------------------------------------------
 
+--
 -- Table structure for table `users`
+--
 
 CREATE TABLE `users` (
   `Id` int(11) NOT NULL,
   `username` varchar(128) NOT NULL,
   `email` varchar(128) NOT NULL,
   `password` varchar(128) NOT NULL,
+  `comfirmPassword` varchar(128) NOT NULL,
   `firstName` text NOT NULL,
   `lastName` text NOT NULL,
   `gender` text NOT NULL,
@@ -80,7 +117,29 @@ CREATE TABLE `users` (
 
 -- --------------------------------------------------------
 
+--
+-- Table structure for table `user_progress`
+--
+
+CREATE TABLE `user_progress` (
+  `user_progress_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `conversation_date` date NOT NULL,
+  `conversation_score` int(11) NOT NULL,
+  `vocabulary_focus_grammar` int(11) NOT NULL,
+  `vocabulary_focus_pronunciation` int(11) NOT NULL,
+  `vocabulary_focus_vocabulary` int(11) NOT NULL,
+  `correction_grammar` int(11) NOT NULL,
+  `correction_pronunciation` int(11) NOT NULL,
+  `correction_vocabulary` int(11) NOT NULL,
+  `activity_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `vocabulary`
+--
 
 CREATE TABLE `vocabulary` (
   `vocab_gane_id` int(11) NOT NULL,
@@ -91,102 +150,158 @@ CREATE TABLE `vocabulary` (
   `points` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
--- --------------------------------------------------------
-
--- Table structure for table `userprogress`
-
-CREATE TABLE `userprogress` (
-  `progress_id` int(11) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `conversations_completed` int(11) NOT NULL,
-  `score` int(11) NOT NULL,
-  `vocabulary_focus` varchar(255) NOT NULL,
-  `corrections_received` varchar(255) NOT NULL,
-  `last_activity_date` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
+--
 -- Indexes for dumped tables
+--
 
+--
 -- Indexes for table `activities`
+--
 ALTER TABLE `activities`
   ADD PRIMARY KEY (`activity_id`);
 
+--
 -- Indexes for table `challenge`
+--
 ALTER TABLE `challenge`
   ADD PRIMARY KEY (`challenge_id`),
   ADD KEY `foreign_key` (`activity_id`) USING BTREE;
 
+--
+-- Indexes for table `chathistory`
+--
+ALTER TABLE `chathistory`
+  ADD PRIMARY KEY (`chathistory_id`),
+  ADD KEY `foreign_keyyy` (`user_id`);
+
+--
+-- Indexes for table `language_analysis`
+--
+ALTER TABLE `language_analysis`
+  ADD PRIMARY KEY (`languageAnalysis_id`),
+  ADD KEY `foreign` (`user_id`);
+
+--
 -- Indexes for table `quiz_questions`
+--
 ALTER TABLE `quiz_questions`
   ADD PRIMARY KEY (`question_id`),
   ADD KEY `foreign_key` (`activity_id`) USING BTREE;
 
+--
 -- Indexes for table `users`
+--
 ALTER TABLE `users`
   ADD PRIMARY KEY (`Id`),
   ADD UNIQUE KEY `email` (`email`);
 
+--
+-- Indexes for table `user_progress`
+--
+ALTER TABLE `user_progress`
+  ADD PRIMARY KEY (`user_progress_id`),
+  ADD KEY `foreign_keyy` (`activity_id`),
+  ADD KEY `foreign-key` (`user_id`);
+
+--
 -- Indexes for table `vocabulary`
+--
 ALTER TABLE `vocabulary`
   ADD PRIMARY KEY (`vocab_gane_id`),
   ADD KEY `foreign_key` (`activity_id`);
 
--- Indexes for table `userprogress`
-ALTER TABLE `userprogress`
-  ADD PRIMARY KEY (`progress_id`),
-  ADD KEY `foreign_key` (`user_id`);
-
--- --------------------------------------------------------
-
+--
 -- AUTO_INCREMENT for dumped tables
+--
 
+--
 -- AUTO_INCREMENT for table `activities`
+--
 ALTER TABLE `activities`
   MODIFY `activity_id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
 -- AUTO_INCREMENT for table `challenge`
+--
 ALTER TABLE `challenge`
   MODIFY `challenge_id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `chathistory`
+--
+ALTER TABLE `chathistory`
+  MODIFY `chathistory_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `language_analysis`
+--
+ALTER TABLE `language_analysis`
+  MODIFY `languageAnalysis_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `quiz_questions`
+--
 ALTER TABLE `quiz_questions`
   MODIFY `question_id` int(11) NOT NULL AUTO_INCREMENT;
 
+--
 -- AUTO_INCREMENT for table `users`
+--
 ALTER TABLE `users`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
+--
+-- AUTO_INCREMENT for table `user_progress`
+--
+ALTER TABLE `user_progress`
+  MODIFY `user_progress_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `vocabulary`
+--
 ALTER TABLE `vocabulary`
   MODIFY `vocab_gane_id` int(11) NOT NULL AUTO_INCREMENT;
 
--- AUTO_INCREMENT for table `userprogress`
-ALTER TABLE `userprogress`
-  MODIFY `progress_id` int(11) NOT NULL AUTO_INCREMENT;
-
--- --------------------------------------------------------
-
+--
 -- Constraints for dumped tables
+--
 
+--
 -- Constraints for table `challenge`
+--
 ALTER TABLE `challenge`
   ADD CONSTRAINT `Test` FOREIGN KEY (`activity_id`) REFERENCES `activities` (`activity_id`);
 
+--
+-- Constraints for table `chathistory`
+--
+ALTER TABLE `chathistory`
+  ADD CONSTRAINT `foreign_keyyy` FOREIGN KEY (`user_id`) REFERENCES `users` (`Id`);
+
+--
+-- Constraints for table `language_analysis`
+--
+ALTER TABLE `language_analysis`
+  ADD CONSTRAINT `foreign` FOREIGN KEY (`user_id`) REFERENCES `users` (`Id`);
+
+--
 -- Constraints for table `quiz_questions`
+--
 ALTER TABLE `quiz_questions`
   ADD CONSTRAINT `Test_quiz` FOREIGN KEY (`activity_id`) REFERENCES `activities` (`activity_id`);
 
+--
+-- Constraints for table `user_progress`
+--
+ALTER TABLE `user_progress`
+  ADD CONSTRAINT `foreign-key` FOREIGN KEY (`user_id`) REFERENCES `users` (`Id`),
+  ADD CONSTRAINT `foreign_keyy` FOREIGN KEY (`activity_id`) REFERENCES `activities` (`activity_id`);
+
+--
 -- Constraints for table `vocabulary`
+--
 ALTER TABLE `vocabulary`
   ADD CONSTRAINT `foreign_key` FOREIGN KEY (`activity_id`) REFERENCES `activities` (`activity_id`);
-
--- Constraints for table `userprogress`
-ALTER TABLE `userprogress`
-  ADD CONSTRAINT `fk_userprogress_users` FOREIGN KEY (`user_id`) REFERENCES `users` (`Id`)
-  ON DELETE CASCADE ON UPDATE CASCADE;
-
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
